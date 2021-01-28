b5acda6d391f24d5771389b98811171b21b5271c
Apollo Tang <tang.apollo@gmail.com>
2021-01-28 01:12:37 -0500

    less is working

--- react-app/src/main.js
+++ react-app/src/main.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+import './main.less'
 const App = () => <div>hello react</div>
-
 const appContainer = document.getElementById('app-container')
 ReactDom.render( <App/>,  appContainer)
 
--- react-app/webpack.config.js
+++ react-app/webpack.config.js
@@ -1,5 +1,6 @@
 const pathResolve = require('path').resolve;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const {getIfUtils, removeEmpty} = require('webpack-config-utils');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const chalk = require('chalk');
@@ -15,9 +16,12 @@ const absPathToSrc = pathResolve(__dirname, 'src');
 const absPathToDist = pathResolve(__dirname, 'dist');
 
 const webpackConfig = (env = {}) => {
-  const _mode = env.mode || 'production';
+  const _mode = (env.dev === true) ? 'development' : 'production';
   const {ifProduction} = getIfUtils(_mode);
 
+  console.log('xxxx _mode: ', _mode)
+  console.log('xxxx env: ', env)
+  console.log('xxxx ifProduction: ', ifProduction('prod', 'dev'))
   const devServer = ifProduction( {}, {
     devServer: {
       host: webpackDevServer_host,
@@ -57,13 +61,29 @@ const webpackConfig = (env = {}) => {
           use: 'babel-loader',
           exclude: /node_modules/
         },
+        ifProduction(
+          {
+            test: /\.(css|less)$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+            exclude: [/node_modules/, /\.module\.(css|less)$/]
+          },
+          {
+            test: /\.(css|less)$/,
+            use: [
+              { loader: "style-loader" },
+              { loader: "css-loader" },
+              { loader: "less-loader", options: { lessOptions: { strictMath: true } } },
+            ],
+            exclude: [/node_modules/, /\.module\.(css|less)$/]
+          }
+        ),
       ])
     },
     plugins: removeEmpty([
-      new ProgressBarPlugin({
-        format: '  build [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)',
-        clear: false
-      }),
+      // new ProgressBarPlugin({
+      //   format: '  build [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)',
+      //   clear: false
+      // }),
       new HtmlWebpackPlugin({
         template: 'index.html',
         title: title
